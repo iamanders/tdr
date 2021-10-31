baecfe092858187d39af3c7dad54de60f1f62ce2
iamanders <anders@iamanders.com>
2021-10-31 09:38:55 +0100

    Added start and stop working time

--- render-helpers.go
+++ render-helpers.go
@@ -12,6 +12,7 @@ import (
 var templateFunctions = template.FuncMap{
 	"timeDurationMultiple": models.TimeDurationMultiple,
 	"formatDuration":       formatDuration,
+	"add":                  add,
 }
 
 // Format time duration with format hh:mm
@@ -19,4 +20,9 @@ func formatDuration(td time.Duration) string {
 	hours := int(math.Floor(td.Hours()))
 	minutes := int(int(td.Minutes()) - (hours * 60))
 	return fmt.Sprintf("%02d:%02d", hours, minutes)
+}
+
+// Addition function for templates
+func add(a int, b int) int {
+	return a + b
 }
